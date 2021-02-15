80136283fd2bde3052ba6076141db5ece48a1708
developer deepu <65069470+sudeepdeep@users.noreply.github.com>
2021-02-15 11:31:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -259,7 +259,7 @@ def attendfirst():
 		today = datetime.today()
 		a = today.month - 1
 		curr_month = months[a]
-		date = date.today()
+		date = today.date()
 		year = today.year
 		try:
 			data = db.child("attendence").child(year).child(curr_month).child(date).child(regno).get()
@@ -303,7 +303,7 @@ def attendcheck():
 	today = datetime.today()
 	a = today.month - 1
 	curr_month = months[a]
-	date = date.today()
+	date = today.date()
 	year = today.year
 	data = db.child("attendence").child(year).child(curr_month).child(date).child(regno).get()
 	for att in data.each():
